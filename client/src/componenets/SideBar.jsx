6c4a5e7063ffc449cc46abccf1db154e0ae1cfcb
Howler234 <howler234@gmail.com>
2023-07-20 10:15:14 -0600

    Adjusted button order

--- client/src/componenets/SideBar.jsx
+++ client/src/componenets/SideBar.jsx
@@ -4,10 +4,11 @@ import {Link} from 'react-router-dom';
 export default function SideBar() {
     return (
     <div className="sideBar">
+        <Link to='/home' className="button glow-button">Home</Link>
         <Link to='/student' className="button glow-button">Student</Link>
         <Link to='/admin' className="button glow-button">Admin</Link>
         <button className="button glow-button">Courses</button>
-        <Link to='/home' className="button glow-button">Home</Link>
+        
         <span></span>
         <span></span>
         <button className="button glow-button">Logout</button>
