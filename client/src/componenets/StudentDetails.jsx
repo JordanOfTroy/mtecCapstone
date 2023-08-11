3269a5e566092295b6f4411c26822f7c2d34517a
Jordan <jordan@jordans-air.lan>
2023-08-11 09:24:42 -0600

    function buttons hooked up to individual functions, state values created.

--- client/src/componenets/StudentDetails.jsx
+++ client/src/componenets/StudentDetails.jsx
@@ -8,6 +8,11 @@ export default function StudentDetails () {
     const [userInfo, setUserInfo] = useState({})
     const [userCourses,setUserCourses] = useState([])
     const {studentId} = useParams()
+    const [isButtonClicked, setIsButtonClicked] = useState(false)
+    const [isRemovingStudent, setIsRemovingStudent] = useState(false)
+    const [isUpdatingStudent, setIsUpdatingStudent] = useState(false)
+    const [isAddingCourse, setIsAddingCourse] = useState(false)
+    const [isRemovingCourse, setIsRemovingCourse] = useState(false)
     
     useEffect(() => {
         const initialApiCall = async () => {
@@ -21,11 +26,9 @@ export default function StudentDetails () {
                     
                 })
                 let results = await rawResults.json()
-                console.log(results)
+                // console.log(results)
                 setUserInfo(results.user[0])
                 setUserCourses(results.courses)
-                console.log(userInfo)
-                console.log(userCourses)
             } catch (err) {
                 console.log('FETCHING ERROR:', err)
             }
@@ -34,33 +37,35 @@ export default function StudentDetails () {
         initialApiCall()
     }, [studentId])
 
-    return (
-        <div className="container">
-            <SideBar/>
-            <div>
-                <Header title='Student Details' />
-                <div className="userInfo">
+    const handleRemoveStudent = () => {
+        console.log('removing student')
+    }
+
+    const handleUpdateStudent = () => {
+        console.log('updating student')
+    }
+    
+    const handleAddNewCourse = () => {
+        console.log('adding course')
+    }
+    
+    const handleRemoveCourse = () => {
+        console.log('removing course')
+    }
+
+
+
+    const initialView = (
+        <>
+        <div className="userInfo">
                     <h2>Student Info</h2>
-                    <div className="userInfoBlock">
-                        <label htmlFor="userName">Student Name:</label>
-                        <p className='userInfo'id='userName'>{userInfo.first_name} {userInfo.last_name}</p>
-                    </div>
-                    <div className="userInfoBlock">
-                        <label htmlFor="userId">Student ID:</label>
-                        <p className='userInfo'id='userId'>{userInfo.id}</p>
-                    </div>
-                    <div className="userInfoBlock">
-                        <label htmlFor="userEmail">Student Email:</label>
-                        <p className='userInfo'id='userEmail'>{userInfo.email}</p>
-                    </div>
-                    <div className="userInfoBlock">
-                        <label htmlFor="userTelephone">Student Phone:</label>
-                        <p className='userInfo'id='userTelephone'>{userInfo.telephone}</p>
-                    </div>
-                    <div className="userInfoBlock">
-                        <label htmlFor="userAddress">Student Address:</label>
-                        <p className='userInfo'id='userAddress'>{userInfo.address}</p>
-                    </div>
+                    <ul>
+                        <li>Name: {userInfo.first_name} {userInfo.last_name}</li>
+                        <li>ID: {userInfo.id}</li>
+                        <li>Email: {userInfo.email}</li>
+                        <li>Phone: {userInfo.telephone}</li>
+                        <li>Address: {userInfo.address}</li>
+                    </ul>
                 </div>
                 <div className="userCourses">
                     <h2>Student Courses</h2>
@@ -68,24 +73,43 @@ export default function StudentDetails () {
                         <table>
                             <tr>
                                 <th>Title</th>
-                                <th></th>
-                                <th></th>
-                                <th></th>
+                                <th>Course Code</th>
+                                <th>Description</th>
                             </tr>
-                        </table>
-                        <ul>
                             {userCourses && userCourses.length > 0 ? (
                                     userCourses.map((course, i) => (
-                                        <li key={i}>{course.title}</li>
+                                        <tr key={i}>
+                                            <td >{course.title}</td>
+                                            <td>{course.course_code}</td>
+                                            <td>{course.description}</td>
+                                        </tr>
                                     ))
                                 ) : (
                                     <p>No courses found.</p>
                                 )
                             }
-                        </ul>
+                        </table>
                     </div>
                 </div>
-                <div className="userCourses"></div>
+                <div className="buttonsOfPower">
+                    <button className="button glow-button" onClick={() => handleRemoveStudent()}>Remove student</button>
+                    <button className="button glow-button" onClick={() => handleUpdateStudent()}>Update student</button>
+                    <button className="button glow-button" onClick={() => handleAddNewCourse()}>Add new course</button>
+                    <button className="button glow-button" onClick={() => handleRemoveCourse()}>Remove course</button>
+                </div>
+        </>
+    )
+    const allOff = !isButtonClicked && !isRemovingStudent && !isUpdatingStudent && !isAddingCourse && !isRemovingCourse
+    return (
+        <div className="container">
+            <SideBar/>
+            <div>
+                <Header title='Student Details' />
+                {
+                    allOff ? initialView : <p>fuck off</p>
+                }
+                {/* {initialView} */}
+                
             </div>
         </div>
     )
