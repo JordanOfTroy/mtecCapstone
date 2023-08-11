3374430192ecd3a7a149be644dbd09eca2dee85e
Jordan <jordan@jordans-air.lan>
2023-08-11 15:15:06 -0600

    there is a bug somewhere in getting available courses and joining courses.

--- client/src/componenets/AddingCourseView.jsx
+++ client/src/componenets/AddingCourseView.jsx
@@ -15,9 +15,7 @@ export default function AddingCourseView ({handleCancel, userInfo}) {
                     }
                 })
                 let parsedCourses = await rawCourses.json()
-                console.log(parsedCourses)
                 setAvailableCourse(parsedCourses)
-                console.log('from useEffect:', availableCourses)
             } catch (err) {
                 console.log('FETCHING ERROR:', err)
             }
@@ -26,22 +24,50 @@ export default function AddingCourseView ({handleCancel, userInfo}) {
     }, [])
 
     const courseOptions = () => {
-        console.log(availableCourses)
         return (
             availableCourses.map((course, i) => {
-                const {title, course_code, description} = course
+                const {title, course_code, description, id} = course
                 return (
                     <tr key={i}>
                         <td>{title}</td>
                         <td>{course_code}</td>
                         <td>{description}</td>
-                        <td><input type="checkbox" className="courseOption" /></td>
+                        <td><input type="checkbox" className="courseOption" value={id}/></td>
                     </tr>
                 )
             })
         )
     }
-    console.log('before Return:', availableCourses)
+
+
+    const handleAddNewCourses = async () => {
+        const courseOptions = document.getElementsByClassName('courseOption');
+        const selectedCourses = [];
+    
+        for (let i = 0; i < courseOptions.length; i++) {
+            if (courseOptions[i].checked) {
+                selectedCourses.push(courseOptions[i].value);
+            }
+        }
+    
+        try {
+            let rawResponse = await fetch('/api/joinCourse', {
+                method: 'PUT',
+                headers: {
+                    "content-type": "application/json",
+                    Authorization: `Bearer ${window.localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({ selectedCourses, id })
+            });
+    
+            console.log(rawResponse.status)
+            let parsedResponse = await rawResponse.json()
+            console.log(parsedResponse)
+        } catch (err) {
+            console.log('FETCHING ERROR:', err)
+        }
+    }
+
     return (
         <>
             <h1>Adding Course</h1>
@@ -58,7 +84,7 @@ export default function AddingCourseView ({handleCancel, userInfo}) {
             </div>
             <div>
                 <button className="button glow-button" onClick={() => handleCancel()}>Cancel</button>
-                <button>Add new courses</button>
+                <button className="button glow-button" onClick={() => handleAddNewCourses()}>Add new courses</button>
             </div>
         </>
     )
